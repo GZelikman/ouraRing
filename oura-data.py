c97e14dd4adfec0023203d9b3024fc54c7b831be
Gregory Zelikman <gregory.zelikman@protonmail.com>
2025-03-20 14:33:21 +0100

    Some nicer design

--- oura-data.py
+++ oura-data.py
@@ -1,20 +1,15 @@
 import requests
 
+def get_data(URL):
+    Header = {'Authorization': "Bearer " + APIToken}
+    r = requests.get(url = URL, headers = Header)
+    return r.json()
+
 file_path = 'APItoken.txt'
 with open(file_path, 'r') as file:
     APIToken = file.read()
 
-URLread = "https://api.ouraring.com/v2/usercollection/daily_readiness"
-URLsleep = "https://api.ouraring.com/v2/usercollection/daily_sleep"
-URLactivity = "https://api.ouraring.com/v2/usercollection/daily_activity"
-Header = {'Authorization': "Bearer " + APIToken}
-
-r_read = requests.get(url = URLread, headers = Header)
-r_activity = requests.get(url = URLactivity, headers = Header)
-r_sleep = requests.get(url = URLsleep, headers = Header)
-
-Readiness = r_read.json()
-Activity = r_activity.json()
-Sleep = r_sleep.json()
-
+Readiness = get_data("https://api.ouraring.com/v2/usercollection/daily_readiness")
+Activity = get_data("https://api.ouraring.com/v2/usercollection/daily_activity")
+Sleep = get_data("https://api.ouraring.com/v2/usercollection/daily_sleep")
 print("Readiness: ", Readiness["data"][0]["score"] , " Sleep: " , Sleep["data"][0]["score"], " Activity: " , Activity["data"][0]["score"])
