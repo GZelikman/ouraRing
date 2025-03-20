72d25fb36d7e4d90aa6e458b6de645c0af1f870b
Gregory Zelikman <gregory.zelikman@protonmail.com>
2025-03-20 14:11:32 +0100

    Access to API works

--- oura-data.py
+++ oura-data.py
@@ -1,5 +1,11 @@
+import requests
 
 file_path = 'APItoken.txt'
 with open(file_path, 'r') as file:
     APIToken = file.read()
-print(APIToken)
+
+URL = "https://api.ouraring.com/v2/usercollection/sleep"
+Header = {'Authorization': "Bearer " + APIToken}
+r = requests.get(url = URL, headers = Header)
+data = r.json()
+print(data)
