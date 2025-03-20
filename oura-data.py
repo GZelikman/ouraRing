67ed1e965b69413ac9351319a48f7a06e0a8258d
Gregory Zelikman <gregory.zelikman@protonmail.com>
2025-03-20 14:18:32 +0100

    Print Readiness Score

--- oura-data.py
+++ oura-data.py
@@ -4,8 +4,8 @@ file_path = 'APItoken.txt'
 with open(file_path, 'r') as file:
     APIToken = file.read()
 
-URL = "https://api.ouraring.com/v2/usercollection/sleep"
+URL = "https://api.ouraring.com/v2/usercollection/daily_readiness"
 Header = {'Authorization': "Bearer " + APIToken}
 r = requests.get(url = URL, headers = Header)
 data = r.json()
-print(data)
+print(data["data"][0]["score"])
