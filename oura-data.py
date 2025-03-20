ef3861aaedf536aabb8a78dc46477d1922113f7f
Gregory Zelikman <gregory.zelikman@protonmail.com>
2025-03-20 14:26:10 +0100

    Added all Main Scores

--- oura-data.py
+++ oura-data.py
@@ -4,8 +4,17 @@ file_path = 'APItoken.txt'
 with open(file_path, 'r') as file:
     APIToken = file.read()
 
-URL = "https://api.ouraring.com/v2/usercollection/daily_readiness"
+URLread = "https://api.ouraring.com/v2/usercollection/daily_readiness"
+URLsleep = "https://api.ouraring.com/v2/usercollection/daily_sleep"
+URLactivity = "https://api.ouraring.com/v2/usercollection/daily_activity"
 Header = {'Authorization': "Bearer " + APIToken}
-r = requests.get(url = URL, headers = Header)
-data = r.json()
-print(data["data"][0]["score"])
+
+r_read = requests.get(url = URLread, headers = Header)
+r_activity = requests.get(url = URLactivity, headers = Header)
+r_sleep = requests.get(url = URLsleep, headers = Header)
+
+Readiness = r_read.json()
+Activity = r_activity.json()
+Sleep = r_sleep.json()
+
+print("Readiness: ", Readiness["data"][0]["score"] , " Sleep: " , Sleep["data"][0]["score"], " Activity: " , Activity["data"][0]["score"])
